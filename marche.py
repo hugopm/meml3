4a477723da08cf1b965d63b2c5ada39ebb27ee15
hugopm <hugo.pm@protonmail.com>
2023-06-09 18:38:14 +0200

    fin marche

--- marche.py
+++ marche.py
@@ -14,6 +14,9 @@ word="()(((()))()(()))(())"
 coords = [(0,0)]
 couplage = []
 pile = []
+
+beamer = 1
+print("\only<%s>{\includegraphics[width=\\textwidth]{marche/all.pdf}}" % (beamer))
 for i in range(len(word)):
     c = word[i]
     x, y = coords[-1]
@@ -22,16 +25,36 @@ for i in range(len(word)):
         pile.append(i)
     else:
         y = y-1
-        couplage.append((pile.pop()*DIM_X/NB_POINTS, (i+1)*DIM_X/NB_POINTS))
+        couplage.append(((pile.pop()+0.5)*DIM_X/NB_POINTS, (i+0.5)*DIM_X/NB_POINTS))
     coords.append((x+DIM_X/NB_POINTS, y))
-    pic = Picture(scale=3, line_cap='round')
     if c == ')':
+        pic = Picture(scale=3, line_cap='round')
         pic.draw((0, 0), grid((DIM_X, DIM_Y), xstep=DIM_X/NB_POINTS, ystep=1),
             help_lines=True, very_thin=True)
         pic.draw(line(coords), thick=True, line_width='0.25mm')
-        print(i, coords, couplage)
+        #print(i, coords, couplage)
         for x1, x2 in couplage[:-1]:
             up(pic, x1, x2, thin=True, color="gray", dashed=True)
         x1, x2 = couplage[-1]
         up(pic, x1, x2, thick=True, color="red")
-        pic.write_image(f"marche_{i}.pdf")
+        pic.write_image(f"marche/{i}.pdf")
+        beamer += 1
+        print("\only<%s>{\includegraphics[width=\\textwidth]{marche/%s.pdf}}" % (beamer, i))
+
+# marche only
+pic = Picture(scale=3, line_cap='round')
+pic.draw((0, 0), grid((DIM_X, DIM_Y), xstep=DIM_X/NB_POINTS, ystep=1),
+    help_lines=True, very_thin=True)
+pic.draw(line(coords), thick=True, line_width='0.25mm')
+pic.write_image("marche/all.pdf")
+
+# couplage en prio
+pic = Picture(scale=3, line_cap='round')
+pic.draw((0, 0), grid((DIM_X, DIM_Y), xstep=DIM_X/NB_POINTS, ystep=1),
+    help_lines=True, very_thin=True)
+pic.draw(line(coords), very_thin=True, color="gray", dashed=True)
+for x1, x2 in couplage:
+    up(pic, x1, x2, thick=True)
+pic.write_image(f"marche/sys.pdf")
+beamer += 1
+print("\only<%s>{\includegraphics[width=\\textwidth]{marche/sys.pdf}}" % (beamer))
